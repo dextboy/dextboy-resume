8629d56f53d1cae891d739427d674d123ddd5782
Tan Dexter <dextboy@outlook.com>
2023-10-10 16:07:55 +0800

    debug 3

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.RESEND_API_KEY;
+const fromEmail = process.env.fromEmail;
 
 export async function POST(req, res) {
   const { body } = await req.json();
