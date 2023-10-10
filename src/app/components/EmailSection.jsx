5acc0ce5174427a2cd5a52c856720e8c9717e605
Tan Dexter <dextboy@outlook.com>
2023-10-10 16:32:10 +0800

    Changed links for github and linkedin

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -47,10 +47,10 @@ const EmailSection = () => {
           to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com/">
+          <Link href="https://github.com/dextboy">
             <Image src={github} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/tan-dexter/">
             <Image src={linkedin} alt="Linkedin Icon" />
           </Link>
         </div>
