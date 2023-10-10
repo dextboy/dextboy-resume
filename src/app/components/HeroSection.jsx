2a530bdd288a003402e66e17ec0d60587bf30b1b
Tan Dexter <dextboy@outlook.com>
2023-10-11 01:58:10 +0800

    debug for download CV

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -48,7 +48,8 @@ const HeroSection = () => {
             </Link>
             <Link
               href="../../../public/dexter-cv.pdf"
-              download="dexter-cv.pdf"
+              target="_blank"
+              rel="noreferrer"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
